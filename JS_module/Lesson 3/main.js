0fd511f6120db77fba421a3e6b59c06175d2cc56
SvitlanaBezeiko <bezeiko93@gmail.com>
2025-02-11 19:52:44 +0200

    1st commit

--- JS_module/Lesson 3/main.js
+++ JS_module/Lesson 3/main.js
@@ -80,3 +80,45 @@ let products = [
         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
     },
 ];
+for (const product of products) {
+    document.write(`<div class="product-card">
+        <h3 class="product-title">${product.title}. Price - ${product.price}</h3>
+        <img src="${product.image}" alt="" class="product-image">
+</div>`)
+}
+
+// за допомоги циклу вивести:
+//     - користувачів зі статусом true
+// - користувачів зі статусом false
+// - користувачів які старші за 30 років
+
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+for (let user of users){
+    if (user.status){  //відразу значення саме по собі тру//
+        console.log(user);
+    }
+}
+
+for (let user of users){
+    if (!user.status){ //не тру, !-заперечення//
+        console.log(user);
+    }
+}
+for (let user of users) {
+    if (user.age > 30){
+        console.log(user);
+    }
+
+}
